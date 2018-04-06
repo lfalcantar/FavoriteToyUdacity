f416e32bd395c466af6236130b00da67bde07d90
lfalcantar <lfalcantar@miners.utep.edu>
2018-04-06 07:02:39 -0700

    Change the inner class to final static

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -4,21 +4,19 @@ import android.provider.BaseColumns;
 
 public class WaitlistContract {
 
-
+    //There are no instance of this class
+    private WaitlistContract(){}
 
     // completed (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-    public class WaitlistEntry implements BaseColumns {
+    public final static class WaitlistEntry implements BaseColumns {
         public static final String TABLE_NAME = "waitlist";
         public static final String COULUMN_GUEST_NAME = "guestName";
         public static final String COULUMN_PARTY_SIZE = "partySize";
         public static final String COULUMN_TIMESTAMP = "timestamp";
 
 
-
-
-
     }
-        // TODO (2) Inside create a static final members for the table name and each of the db columns
+        //  (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
