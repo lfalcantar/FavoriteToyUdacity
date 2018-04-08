ae85141027f6838b545752f94349dd3ba449d913
lfalcantar <lfalcantar@miners.utep.edu>
2018-04-08 13:14:58 -0700

    add to database

--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -16,16 +16,23 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.example.android.todolist.data.TaskContract;
 
 
 public class AddTaskActivity extends AppCompatActivity {
 
     // Declare a member variable to keep track of a task's selected mPriority
     private int mPriority;
+    private EditText mtaskDescription;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,8 @@ public class AddTaskActivity extends AppCompatActivity {
         // Initialize to highest mPriority by default (mPriority = 1)
         ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
         mPriority = 1;
+
+        mtaskDescription = (EditText) findViewById(R.id.editTextTaskDescription);
     }
 
 
@@ -44,13 +53,24 @@ public class AddTaskActivity extends AppCompatActivity {
      */
     public void onClickAddTask(View view) {
         // Not yet implemented
-        // TODO (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        // completed (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        String input;
+        if((input = mtaskDescription.getText().toString()).isEmpty()){
+            return;
+        }
+        // completed (7) Insert new task data via a ContentResolver
 
-        // TODO (7) Insert new task data via a ContentResolver
+        // completed (8) Display the URI that's returned with a Toast
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, input);
+        contentValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
+        // Insert the content values via a ContentResolver
+        Uri uri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, contentValues);
+        if(uri != null) {
+            Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+        }
 
-        // TODO (8) Display the URI that's returned with a Toast
-        // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
-
+        finish();
     }
 
 
